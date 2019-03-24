b0fbb2d8f6d2c216a3a467d397c479f2e45a6e2b
Katerina Zhadko <katerinkaj@icloud.com>
2019-03-24 23:43:35 +0300

    feat: add cycle to check pairs (tests: 13 passed, 7 failed)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  
-  if(str.length % 2 != 0){
-    return false;
-}
 
+  if (str.length % 2 != 0) {
+    return false;
+  }
+
+  let checkString="";
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+
+    if (str.includes(bracketsConfig[i])) {
+
+      checkString = str.replace(bracketsConfig[i], '');
+    }
+    
+  }
+
+return (checkString.length == 0) ? true : false;
+  
 }
